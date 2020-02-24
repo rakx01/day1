5a0f84a7eaf821374d15c02c7bc4376505f93efa
rakx01 <zhenechka.rak@mail.ru>
2020-02-24 12:44:04 +0300

    version2

---
+++ day1/Monitor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day1
+{
+    class Monitor
+    {
+        public string Firm { get; set; }
+        public string Type { get; set; }
+        public float Diagonal { get; set; }
+    }
+}

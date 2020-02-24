3425826ce9260ce6c2f5732986abd9247d9977ab
rakx01 <zhenechka.rak@mail.ru>
2020-02-24 12:46:59 +0300

    version3

--- day1/Monitor.cs
+++ day1/Monitor.cs
@@ -10,4 +10,31 @@ namespace day1
         public string Type { get; set; }
         public float Diagonal { get; set; }
     }
+    public Monitor()
+    {
+        this.Firm = "Нет имени";
+        this.Type = "Нет имени";
+        this.Diagonal = 0;
+    }
+    public Monitor(string firm, string type, float diagonal)
+    {
+        this.Firm = firm;
+        this.Type = type;
+        this.Diagonal = diagonal;
+    }
+    public void Mon()
+    {
+        Console.WriteLine("Фирма:" + Firm + " " + "Тип:" + Type + " " + "Диагональ:" + Diagonal + " ");
+    }
+    public void Input()
+    {
+        Console.WriteLine("Введите фирму: ");
+        Firm = Console.ReadLine();
+        Console.WriteLine("Введите тип: ");
+        Type = Console.ReadLine();
+        Console.WriteLine("Введите диагональ: ");
+        Diagonal = int.Parse(Console.ReadLine());
+    }
+}
+}
 }
